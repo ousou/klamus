a5b3558bbdb4a76e882b16dbcb961ad0dd786e98
Sebastian Björkqvist <sebastian@sebastianbjorkqvist.com>
2017-07-10 20:27:03 +0300

    Add save method to RecordController

--- klamus/src/main/java/klamus/restcontroller/RecordController.java
+++ klamus/src/main/java/klamus/restcontroller/RecordController.java
@@ -4,10 +4,16 @@ import klamus.entity.Record;
 import klamus.repository.RecordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 /**
  *
@@ -33,4 +39,16 @@ public class RecordController {
     public Record getById(@PathVariable String id) {
         return repository.findOne(Long.parseLong(id));
     }       
+    
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<?> save(@RequestBody Record soloist) {
+
+        Record savedRecotd = repository.save(soloist);
+        
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(ServletUriComponentsBuilder
+                        .fromCurrentRequest().path("/{id}")
+                        .buildAndExpand(savedRecotd.getRecordId()).toUri());
+        return new ResponseEntity<>(null, httpHeaders, HttpStatus.CREATED);        
+    }    
 }
