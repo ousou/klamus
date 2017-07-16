33553f85be370df6e2d8fe1198c90e66a45c16f4
Sebastian Björkqvist <sebastian@sebastianbjorkqvist.com>
2017-07-16 16:23:32 +0300

    Add default CORS mappings

---
+++ klamus/src/main/java/klamus/KlamusConfiguration.java
@@ -0,0 +1,25 @@
+package klamus;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ *
+ * @author Sebastian Björkqvist <sebastian@sebastianbjorkqvist.com>
+ */
+@Configuration
+public class KlamusConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+}
